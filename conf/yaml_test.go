1421d5ead7333d894249941967fdbae5cd033432
odacremolbap <odacremolbap@gmail.com>
2016-05-25 22:26:40 +0200

    If duplicated dependencies, use the first appearance.
    Fixes #29

---
+++ conf/yaml_test.go
@@ -0,0 +1,51 @@
+package conf
+
+import (
+	"testing"
+)
+
+func TestDuplicates(t *testing.T) {
+
+	testData := []struct {
+		imports    []Import
+		duplicates int
+	}{
+		{[]Import{
+			{"package1", "version1", ""},
+		}, 0},
+		{[]Import{
+			{"package1", "version1", ""},
+			{"package2", "version1", "repoA"},
+		}, 0},
+		{[]Import{
+			{"package1", "version1", ""},
+			{"package2", "version1", "repoA"},
+			{"package1", "version1", ""},
+		}, 1},
+		{[]Import{
+			{"package1", "version1", ""},
+			{"package2", "version1", "repoA"},
+			{"package1", "version1", ""},
+			{"package1", "version1", ""},
+		}, 2},
+		{[]Import{
+			{"package1", "version1", ""},
+			{"package2", "version1", "repoA"},
+			{"package1", "version1", ""},
+			{"package1", "version1", ""},
+			{"package2", "version2", "repoB"},
+			{"package3", "version1", "repoA"},
+		}, 3},
+	}
+
+	for i, d := range testData {
+		trash := Trash{"", d.imports}
+		trash.deleteDups()
+
+		if d.duplicates != len(d.imports)-len(trash.Imports) {
+			t.Errorf("Case %d failed: expected %d duplicates but removed %d", i, d.duplicates, len(d.imports)-len(trash.Imports))
+		}
+
+	}
+
+}
